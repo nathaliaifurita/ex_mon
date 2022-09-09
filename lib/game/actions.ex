0447e49b79b5e82d3991323d6073011a7ad7defb
nathaliaifurita <nathaliaofreire@gmail.com>
2022-09-09 18:35:40 -0300

    Calculando o valor de ataque

--- lib/game/actions.ex
+++ lib/game/actions.ex
@@ -1,14 +1,23 @@
 defmodule ExMon.Game.Actions do
-    alias ExMon.Game
+  alias ExMon.Game
+  alias ExMon.Game.Actions.Attack
 
-    def fetch_move(move) do
-        Game.player()
-        |> Map.get(:moves)
-        |> find_moves(move)
+  def attack(move) do
+    case Game.turn() do
+      :player -> Attack.attack_opponent(:computador, move)
+      :computer -> Attack.attack_opponent(:player, move)
     end
+  end
 
-    defp find_moves(moves, move) do
-        Enum.find_value(moves, {:error, move}, fn {key, value} ->
-            if value == move, do: {:ok, key} end)
-    end
+  def fetch_move(move) do
+    Game.player()
+    |> Map.get(:moves)
+    |> find_moves(move)
+  end
+
+  defp find_moves(moves, move) do
+    Enum.find_value(moves, {:error, move}, fn {key, value} ->
+      if value == move, do: {:ok, key}
+    end)
+  end
 end
--- lib/game/status.ex
+++ lib/game/status.ex
@@ -1,9 +1,13 @@
 defmodule ExMon.Game.Status do
-    alias ExMon.Game
+  alias ExMon.Game
 
-    def print_rount_message() do
-        IO.puts("======The game is started!======\n")
-        IO.inspect(Game.info())
-        IO.puts("-------------------------\n")
-    end
+  def print_rount_message() do
+    IO.puts("======The game is started!======\n")
+    IO.inspect(Game.info())
+    IO.puts("-------------------------\n")
+  end
+
+  def print_wrong_move_message(move) do
+    IO.puts("======Invalid move: #{move}!======\n")
+  end
 end
