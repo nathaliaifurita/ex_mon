b7ae595cfca658a618c997ef244af15da784ea7b
Nathalia Freire <nathaliaofreire@gmail.com>
2023-01-20 16:31:09 -0300

    Changing turns from computer to player and player to computer

--- lib/game.ex
+++ lib/game.ex
@@ -1,4 +1,5 @@
 defmodule ExMon.Game do
+  alias ExMon.Player
   use Agent
 
   def start(computer, player) do
@@ -18,10 +19,28 @@ defmodule ExMon.Game do
 
   # vai receber um estado e vai subscreber com a função update 
   def update(state) do
-    Agent.update(__MODULE__, fn _ -> state end)
+    Agent.update(__MODULE__, fn _ -> update_game_status(state) end)
   end
 
   def player, do: Map.get(info(), :player)
   def turn, do: Map.get(info(), :turn)
   def fetch_player(player), do: Map.get(info(), player)
+
+  # pattern matching a chave player que é uma struct do tipo player, e a struct tem uma chave life
+  defp update_game_status(%{
+         player: %Player{life: player_life},
+         computer: %Player{life: computer_life} = state
+       })
+       when player_life == 0 or computer_life == 0,
+       do: Map.put(state, :status, :game_over)
+
+  defp update_game_status(state) do
+    state
+    |> Map.put(:status, :continue)
+    |> update_turn()
+  end
+
+  # do estado atual, eu vou mudar de player pra comp ou comp pra player
+  defp update_turn(%{turn: :player} = state), do: Map.put(state, :turn, :computer)
+  defp update_turn(%{turn: :computer} = state), do: Map.put(state, :turn, :player)
 end
